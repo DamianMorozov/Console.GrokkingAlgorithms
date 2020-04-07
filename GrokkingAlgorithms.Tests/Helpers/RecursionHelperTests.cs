8546d6497741430b0b5dd44092e8a7fcf43a7da9
Morozov Damian <damian@morozov33.ru>
2020-04-07 16:07:43 +0300

    Updated RecursionHelperTests

--- GrokkingAlgorithms.Tests/Helpers/RecursionHelperTests.cs
+++ GrokkingAlgorithms.Tests/Helpers/RecursionHelperTests.cs
@@ -1,4 +1,7 @@
-﻿using GrokkingAlgorithms.Helpers;
+﻿// This is an independent project of an individual developer. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
+
+using GrokkingAlgorithms.Helpers;
 using NUnit.Framework;
 using System.Diagnostics;
 	
